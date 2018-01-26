c6d91bf850142affeeab83cababf2ec5ba341e13
George Gore <georgelgore@gmail.com>
2018-01-26 10:35:11 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,19 @@
 export default function createStore(reducer) {
   // add your code here
+  let state = 0;
+
+  const dispatch = (action = { type: "@@INIT" }) => {
+    state = reducer(state, action);
+    render();
+  };
+
+  let getState = () => {
+    return state;
+  };
+
+  return { dispatch, getState };
 }
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById("container");
 }
